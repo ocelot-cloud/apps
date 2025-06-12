15931116e6ab3b2eeba6fa030b73822079b3a942
Christoph Baier <chrisby@crispy-coding.org>
2025-06-12 10:05:04 +0200

    refactoring

--- updater/go.mod
+++ updater/go.mod
@@ -1,3 +1,10 @@
-module apps
+module updater
 
 go 1.24.1
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/ocelot-cloud/task-runner v0.0.22 // indirect
+	github.com/spf13/cobra v1.9.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+)
--- updater/main.go
+++ updater/main.go
@@ -1,5 +1,64 @@
 package main
 
+import (
+	tr "github.com/ocelot-cloud/task-runner"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	updaterDir = getCurrentDir()
+	projectDir = updaterDir + "/.."
+	appsDir    = projectDir + "/apps/production"
+)
+
+func getCurrentDir() string {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		tr.CleanupAndExitWithError()
+	}
+	return currentDir
+}
+
 func main() {
-	println("Hello, world!")
+	tr.HandleSignals()
+	tr.PrintTaskDescription("This is a test task for the CI runner")
+
+	rootCmd.AddCommand(testUnitsCmd)
+	rootCmd.CompletionOptions = cobra.CompletionOptions{DisableDefaultCmd: true}
+
+	tr.Execute("docker compose up -d")
+
+	if err := rootCmd.Execute(); err != nil {
+		tr.ColoredPrintln("error: %v", err)
+		tr.CleanupAndExitWithError()
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "ci-runner",
+	Short: "CI runner for liquid",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+var testUnitsCmd = &cobra.Command{
+	Use:   "test",
+	Short: "execute updater unit tests",
+	Run: func(cmd *cobra.Command, args []string) {
+		tr.PrintTaskDescription("execute unit tests")
+		tr.ExecuteInDir(updaterDir, "go test -count=1 ./...")
+	},
+}
+
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "updating production apps",
+	Run: func(cmd *cobra.Command, args []string) {
+		tr.PrintTaskDescription("updating production apps")
+		// TODO conduct update
+		// TODO conduct setup and stability test
+		// TODO printing report
+	},
 }
