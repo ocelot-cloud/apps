53e9eb64a08872383443441861f040ebea173279
Christoph Baier <chrisby@crispy-coding.org>
2025-06-13 11:25:11 +0200

    mocked updater created

--- updater/updater.go
+++ updater/updater.go
@@ -11,15 +11,86 @@ print summary
 
 //go:generate mockery
 type FileSystemOperator interface {
+	// needed for healthchecks and updates
 	GetListOfApps(appsDir string) ([]string, error)
+	GetPortOfApp(appDir string) (string, error)
 	InjectPortInDockerCompose(appDir string) error
-	RunDockerCompose(appDir string) error
+	RunInjectedDockerCompose(appDir string) error
+
+	// needed for updates
+	GetImagesOfApp(appDir string) ([]Service, error)
+	WriteNewTagToDockerCompose(appDir, serviceName, newTag string) error
+}
+
+//go:generate mockery
+type EndpointChecker interface {
+	TryAccessingIndexPageOnLocalhost(port string) error
+}
+
+type Service struct {
+	Name  string
+	Image string // TODO not sure if needed
+	Tag   string
 }
 
 type Updater struct {
 	appsDir            string
 	fileSystemOperator FileSystemOperator
-	dockerHubClient    DockerHubClient
+	endpointChecker    EndpointChecker
+}
+
+type HealthCheckReport struct {
+	AllAppsHealthy bool
+	AppReports     []AppHealthReport
+}
+
+type AppHealthReport struct {
+	AppName      string
+	Healthy      bool
+	ErrorMessage string
+}
+
+func (u *Updater) PerformHealthCheck() (*HealthCheckReport, error) {
+	apps, err := u.fileSystemOperator.GetListOfApps(u.appsDir)
+	if err != nil {
+		return nil, err
+	}
+
+	report := HealthCheckReport{
+		AllAppsHealthy: true,
+	}
+	for _, app := range apps {
+		appDir := u.appsDir + "/" + app
+		port, err := u.fileSystemOperator.GetPortOfApp(appDir)
+		if err != nil {
+			// TODO write to report
+			continue
+		}
+		err = u.fileSystemOperator.InjectPortInDockerCompose(appDir)
+		if err != nil {
+			// TODO write to report
+			continue
+		}
+		err = u.fileSystemOperator.RunInjectedDockerCompose(appDir)
+		if err != nil {
+			// TODO write to report
+			continue
+		}
+		err = u.endpointChecker.TryAccessingIndexPageOnLocalhost(port)
+		if err != nil {
+			// TODO write to report
+			continue
+		}
+		report.AppReports = append(report.AppReports, AppHealthReport{
+			AppName:      app,
+			Healthy:      true,
+			ErrorMessage: "",
+		})
+	}
+	return &report, nil
 }
 
 // TODO update, same as above, but we update images before performing healthcheck
+func (u *Updater) PerformUpdate() (*HealthCheckReport, error) {
+	return nil, nil
+}
--- updater/updater_test.go
+++ updater/updater_test.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"github.com/ocelot-cloud/shared/assert"
+	"testing"
+)
+
+func TestUpdater_PerformHealthCheck(t *testing.T) {
+	mockAppsDir := "/test_apps_dir"
+	fileSystemOperatorMock := NewFileSystemOperatorMock(t)
+	endpointCheckerMock := NewEndpointCheckerMock(t)
+	updater := Updater{
+		appsDir:            mockAppsDir,
+		fileSystemOperator: fileSystemOperatorMock,
+		endpointChecker:    endpointCheckerMock,
+	}
+
+	appDir := mockAppsDir + "/sampleapp"
+	fileSystemOperatorMock.On("GetListOfApps", mockAppsDir).Return([]string{"sampleapp"}, nil)
+	fileSystemOperatorMock.On("GetPortOfApp", appDir).Return("8080", nil)
+	fileSystemOperatorMock.On("InjectPortInDockerCompose", appDir).Return(nil)
+	fileSystemOperatorMock.On("RunInjectedDockerCompose", appDir).Return(nil)
+	endpointCheckerMock.On("TryAccessingIndexPageOnLocalhost", "8080").Return(nil)
+
+	report, err := updater.PerformHealthCheck()
+	assert.Nil(t, err)
+	assert.True(t, report.AllAppsHealthy)
+	assert.Equal(t, 1, len(report.AppReports))
+	appReport := report.AppReports[0]
+	assert.Equal(t, "sampleapp", appReport.AppName)
+	assert.True(t, appReport.Healthy)
+
+	fileSystemOperatorMock.AssertExpectations(t)
+	endpointCheckerMock.AssertExpectations(t)
+}
