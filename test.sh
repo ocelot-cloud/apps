6729aa65f4a513e01103097204e0ec126aae521a
Christoph Baier <chrisby@crispy-coding.org>
2025-06-27 19:48:00 +0200

    fix ci

--- test.sh
+++ test.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 cd updater
 go generate
+wire
 go test -count=1 ./...
--- updater/main.go
+++ updater/main.go
@@ -1,40 +1,11 @@
-//go:build wireinject
-// +build wireinject
-
 package main
 
 import (
 	"fmt"
-	"github.com/ocelot-cloud/shared/utils"
-	"log"
-	"os"
-	"path/filepath"
-
 	tr "github.com/ocelot-cloud/task-runner"
 	"github.com/spf13/cobra"
+	"log"
 )
-
-var logger = utils.ProvideLogger("info")
-
-var (
-	updaterDir = getCurrentDir()
-	projectDir = updaterDir + "/.."
-	appsDir    string
-	testDir    = projectDir + "/apps/test"
-)
-
-func init() {
-	defaultDir := filepath.Join(projectDir, "apps/production")
-	rootCmd.PersistentFlags().StringVarP(&appsDir, "path-apps-dir", "p", defaultDir, "directory containing app definitions")
-}
-
-func getCurrentDir() string {
-	dir, err := os.Getwd()
-	if err != nil {
-		tr.CleanupAndExitWithError()
-	}
-	return dir
-}
 
 func main() {
 	tr.HandleSignals()
