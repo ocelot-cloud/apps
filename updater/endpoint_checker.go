1cd20ac710daa0c859467fbfbf996c09674f469c
Christoph Baier <chrisby@crispy-coding.org>
2025-06-27 15:03:35 +0200

    refactoring

---
+++ updater/endpoint_checker.go
@@ -0,0 +1,8 @@
+package main
+
+type EndpointCheckerImpl struct{}
+
+func (e EndpointCheckerImpl) TryAccessingIndexPageOnLocalhost(port string) error {
+	//TODO implement me
+	panic("implement me")
+}
