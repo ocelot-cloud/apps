94edfa6c44b72ea4b4c151143d30d7e97f58fa8b
Christoph Baier <chrisby@crispy-coding.org>
2025-06-28 09:31:11 +0200

    refactoring

--- updater/endpoint_checker.go
+++ updater/endpoint_checker.go
@@ -1,9 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
 type EndpointCheckerImpl struct{}
 
-// TODO expected content (from updater.yml), add timeout (two minutes?)
 func (e EndpointCheckerImpl) TryAccessingIndexPageOnLocalhost(port string, path string) error {
-	//TODO implement me
-	panic("implement me")
+	url := fmt.Sprintf("http://localhost:%s%s", port, path)
+	timeout := 2 * time.Minute
+	deadline := time.Now().Add(timeout)
+	client := http.Client{Timeout: 5 * time.Second}
+
+	for {
+		resp, err := client.Get(url)
+		if err == nil {
+			resp.Body.Close()
+			if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+				return nil
+			}
+			return fmt.Errorf("unexpected status: %s", resp.Status)
+		}
+
+		if time.Now().After(deadline) {
+			return fmt.Errorf("timeout reaching %s: %w", url, err)
+		}
+		time.Sleep(2 * time.Second)
+	}
 }
--- updater/endpoint_checker_test.go
+++ updater/endpoint_checker_test.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestTryAccessingIndexPageOnLocalhost(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer s.Close()
+
+	port := s.URL[len("http://localhost:"):]
+	ec := EndpointCheckerImpl{}
+	err := ec.TryAccessingIndexPageOnLocalhost(port, "/")
+	require.NoError(t, err)
+}
