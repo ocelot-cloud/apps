623ac94b5166e50b40e677eb3b800840424db955
Christoph Baier <chrisby@crispy-coding.org>
2025-06-29 12:13:45 +0200

    refactoring

--- updater/update_applier_test.go
+++ updater/update_applier_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"github.com/ocelot-cloud/shared/assert"
 	"testing"
 )
@@ -20,6 +21,41 @@ func TestAppUpdater_Success(t *testing.T) {
 	actualUpdate, err := updateApplier.ApplyUpdate(sampleAppDir)
 	assert.Nil(t, err)
 	assert.Equal(t, sampleUpdate, actualUpdate)
+}
+
+func TestAppUpdater_FetchFailed(t *testing.T) {
+	setup(t)
+	defer assertAppUpdaterMockExpectations(t)
+
+	appUpdateFetcherMock.EXPECT().Fetch(sampleAppDir).Return(nil, errors.New("some error"))
+
+	_, err := updateApplier.ApplyUpdate(sampleAppDir)
+	assert.Equal(t, "some error", err.Error())
+}
+
+func TestAppUpdater_NoUpdateFound(t *testing.T) {
+	setup(t)
+	defer assertAppUpdaterMockExpectations(t)
+
+	sampleUpdate := getSampleAppUpdate()
+	sampleUpdate.WasUpdateFound = false
+	appUpdateFetcherMock.EXPECT().Fetch(sampleAppDir).Return(sampleUpdate, nil)
+
+	actualUpdate, err := updateApplier.ApplyUpdate(sampleAppDir)
+	assert.Nil(t, err)
+	assert.Equal(t, sampleUpdate, actualUpdate)
+}
+
+func TestAppUpdater_WriteFails(t *testing.T) {
+	setup(t)
+	defer assertAppUpdaterMockExpectations(t)
+
+	sampleUpdate := getSampleAppUpdate()
+	appUpdateFetcherMock.EXPECT().Fetch(sampleAppDir).Return(sampleUpdate, nil)
+	fileSystemOperatorMock.EXPECT().WriteServiceUpdatesIntoComposeFile(sampleAppDir, sampleUpdate.ServiceUpdates).Return(errors.New("some error"))
+
+	_, err := updateApplier.ApplyUpdate(sampleAppDir)
+	assert.Equal(t, "some error", err.Error())
 }
 
 func setup(t *testing.T) {
